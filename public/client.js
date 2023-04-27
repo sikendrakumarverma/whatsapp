c2b820e53b68957b7bd88a8130d9c6e0768d3219
sikendrakumarverma <grdskverma2018@gmail.com>
2023-04-27 12:38:48 +0530

    ok

---
+++ public/client.js
@@ -0,0 +1,107 @@
+// connection with backend socket server
+const socket = io();
+var name;
+
+var chats = document.querySelector('.chats');
+var usersList = document.querySelector('.users-list');
+var usersCount = document.querySelector('.users-count');
+var userMsg = document.querySelector('#user-msg');
+var userSend = document.querySelector('#user-send');
+
+// Audio that will play on sendinging message
+var audio = new Audio('ting.mp3');
+
+// Ask new user name for joining
+do {
+    name = prompt('Please enter your name: ');
+} while (!name);
+
+// send an event to backend with user name
+socket.emit('new-user-joined', name);
+
+// it will called when new user joined
+socket.on('user-connected', (joinedUser) => {
+    userJoinLeft(joinedUser, 'joined');
+});
+
+// function for uer join or left chat
+function userJoinLeft(name, status) {
+    let div = document.createElement('div');
+    div.classList.add('user-join');
+    let content = `<p><b>${name}</b> ${status} the chat</p>`
+    div.innerHTML = content;
+    chats.appendChild(div);
+}
+// it will called when user left
+socket.on('user-disconnected', (userLeft) => {
+    userJoinLeft(userLeft, 'left');
+});
+
+// for updating users list and users count
+socket.on('user-list', (allUsers) => {
+    usersList.innerHTML = '';
+    usersArr = Object.values(allUsers);
+    for (let i = 0; i < usersArr.length; i++) {
+        let p = document.createElement('p');
+        p.innerHTML = usersArr[i];
+        usersList.appendChild(p);
+    }
+    usersCount.innerHTML = usersArr.length;
+});
+
+// for sending messages by click
+userSend.addEventListener('click', (e) => {
+    let data = {
+        user: name,
+        msg: userMsg.value.trim()
+    };
+    if (userMsg.value != '') {
+        appendMessage(data, 'outgoing');
+        // sending an event for backend
+        socket.emit('message', data);
+        audio.play();
+        userMsg.value = "";
+        scrollToBottom();
+    }
+});
+
+// for sending messages by pressing Enter key
+userMsg.addEventListener('keyup', (e) => {
+    let data = {
+        user: name,
+        msg: userMsg.value.trim()
+    };
+    if (userMsg.value != '') {
+        if (e.key === 'Enter') {
+            appendMessage(data, 'outgoing');
+            // sending an event for backend
+            socket.emit('message', data);
+            audio.play();
+            userMsg.value = "";
+            scrollToBottom();
+        }
+    }
+});
+
+// Adding sending or recieving data in div
+function appendMessage(data, status) {
+    let div = document.createElement('div');
+    div.classList.add('message', status);
+    let contents = `
+  <h5>${data.user}</h5>
+  <p>${data.msg}</p>
+  `;
+    div.innerHTML = contents;
+    chats.appendChild(div);
+}
+
+// recieving message from backend
+socket.on('message', (data) => {
+    appendMessage(data, 'incoming');
+    scrollToBottom();
+});
+
+// for scrolling
+function scrollToBottom() {
+    chats.scrollTop = chats.scrollHeight
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const port = process.env.PORT || 8080
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+//socket.io setup
+
+const io = require('socket.io')(http);
+let users = {};
+
+// when a user join cache all events by .on function
+io.on('connection', function (socket) {
+    // when a new user join
+    socket.on('new-user-joined', function (userName) {
+        users[socket.id] = userName; // storing users information in users.
+        // send an event by name of user-connected for already Connected users
+        socket.broadcast.emit('user-connected', userName);
+        // Globally send an event for users list and count
+        io.emit('user-list', users);
+    });
+
+    // when a user leave or disconnect the chat
+    socket.on('disconnect', function () {
+        // send an event user-connected for Connected users
+        socket.broadcast.emit('user-disconnected', user = users[socket.id]);
+        delete users[socket.id]; // deleting users information from users.
+        // Globally send an event for users list and count
+        io.emit('user-list', users);
+    });
+
+    // Receiving message from client
+    socket.on('message', (data) => {
+        // Sending message to all users
+        socket.broadcast.emit('message', { user: data.user, msg: data.msg });
+    });
+});
+
+http.listen(port, () => {
+    console.log("App is running on port", port);
+});
